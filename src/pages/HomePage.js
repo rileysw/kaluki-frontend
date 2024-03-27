aa22e5e295a90ea787630e59c272eb4e9ef64957
Riley Wong <rileysw@uci.edu>
2024-03-26 22:13:36 -0700

    add basic home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import useWebSocket from "react-use-websocket";
+
 function HomePage() {
-  return <p>This is the Home page.</p>;
+  const navigate = useNavigate();
+  const { lastJsonMessage } = useWebSocket(
+    "ws://localhost:8000/get_ready_players"
+  );
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    setPlayers(JSON.parse(lastJsonMessage));
+  }, [lastJsonMessage]);
+
+  const handleSubmit = (event) => {
+    fetch("http://localhost:8000/add_player/" + event.target[0].value)
+      .then(() => {
+        navigate("/ready");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    event.preventDefault();
+  };
+
+  return (
+    <Container>
+      <h1>Kaluki</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Control placeholder="Insert Name" />
+        <Button type="submit">Submit</Button>
+      </Form>
+      <h2>Players Ready</h2>
+      {players !== null &&
+        players.map((name) => {
+          return <p>{name}</p>;
+        })}
+    </Container>
+  );
 }
 
 export default HomePage;
