1b6cf79b724f983032aff5b676c3c86a4a2732d0
Riley Wong <rileysw@uci.edu>
2024-04-10 22:15:07 -0700

    add hand view

--- src/pages/PlayPage.js
+++ src/pages/PlayPage.js
@@ -1,36 +1,91 @@
-import { forwardRef } from "react";
+import { useEffect, useState } from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
-import { motion } from "framer-motion";
-
-const Card = forwardRef((props, ref) => {
-  return (
-    <Image
-      src={require("../assets/red_joker.png")}
-      style={{ backgroundColor: "white", width: "5%" }}
-      props={props}
-      ref={ref}
-      draggable={false}
-    />
-  );
-});
-
-const MotionCard = motion(Card);
+import { useLocation } from "react-router-dom";
+import { Reorder } from "framer-motion";
 
 function PlayPage() {
+  const location = useLocation();
+  const [players, setPlayers] = useState([]);
+  const [hand, setHand] = useState([]);
+
+  const images = require.context("../assets", true);
+  const CardImage = ({ name }) => {
+    let imageSrc = "./" + name + ".png";
+    return (
+      <Image
+        src={images(imageSrc)}
+        style={{ backgroundColor: "white", width: "5vw" }}
+        draggable={false}
+      />
+    );
+  };
+
+  useEffect(() => {
+    // initialize players
+    let playerOrder = [];
+    for (
+      let i = location.state.players.indexOf(location.state.user);
+      i < location.state.players.length;
+      i++
+    ) {
+      playerOrder.push(location.state.players[i]);
+    }
+    for (
+      let i = 0;
+      i < location.state.players.indexOf(location.state.user);
+      i++
+    ) {
+      playerOrder.push(location.state.players[i]);
+    }
+    setPlayers(playerOrder);
+
+    // initialize hand
+    fetch("http://localhost:8000/player_hand/" + location.state.user)
+      .then((res) => res.json())
+      .then((data) => setHand(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Container fluid style={{ height: "100vh" }}>
       <Row style={{ height: "30%" }}>
-        <Col style={{ textAlign: "center" }}>Player 2</Col>
+        <Col style={{ textAlign: "center" }}>
+          <h5>{players[2]}</h5>
+        </Col>
       </Row>
       <Row style={{ height: "30%" }}>
-        <Col>Player 1</Col>
-        <Col style={{ textAlign: "center" }}>Deck</Col>
-        <Col style={{ textAlign: "right" }}>Player 3</Col>
+        <Col>
+          <h5>{players[1]}</h5>
+        </Col>
+        <Col style={{ textAlign: "center" }}>
+          <CardImage name="back@2x" />
+        </Col>
+        <Col style={{ textAlign: "right" }}>
+          <h5>{players[3]}</h5>
+        </Col>
       </Row>
       <Row style={{ height: "40%" }}>
         <Col style={{ textAlign: "center" }}>
-          <h5>Player 4</h5>
-          <MotionCard drag />
+          <h5>{players[0]}</h5>
+          <Reorder.Group
+            axis="x"
+            values={hand}
+            onReorder={setHand}
+            style={{ listStyleType: "none" }}
+          >
+            {hand !== null &&
+              hand.map((card) => {
+                return (
+                  <Reorder.Item
+                    key={card}
+                    value={card}
+                    style={{ display: "inline-block" }}
+                  >
+                    <CardImage name={card[0]} />
+                  </Reorder.Item>
+                );
+              })}
+          </Reorder.Group>
         </Col>
       </Row>
     </Container>
